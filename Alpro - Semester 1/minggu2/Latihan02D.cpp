29f96d3f8e2c1f3b8c5f8eade9332aa2a2041197
Chiro <ficosaputro@gmail.com>
2024-06-27 14:48:39 +0700

    Mengupload folder minggu 2

---
+++ Alpro - Semester 1/minggu2/Latihan02D.cpp
@@ -0,0 +1,25 @@
+//NIM   : A12.2023.07058
+//Nama  : Fico Aldi Saputro
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+float luas;
+int jari_jari;
+float pi = 3.14;
+
+int main()
+{
+    //input............
+    cout << "Input jari-jari = ";
+    cin >> jari_jari;
+
+    //proses...........
+    luas = pi * jari_jari * jari_jari;
+
+    //output...........
+    cout << "Luas = " << luas << endl;
+    getch();
+}
---
+++ Alpro - Semester 1/minggu2/Latihan02e.cpp
@@ -0,0 +1,21 @@
+//NIM   : A12.2023.07058
+//Nama  : Fico Aldi Saputro
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+//deklarasi variabel
+int C,F;
+
+int main()
+{
+    cout << "Input C = ";
+    cin >> C;
+
+    F = (9/5 * C) + 32;
+
+    cout<< "Output F = " << F << endl;
+    getch ();
+}
