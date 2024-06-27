ca79caf7f2e55ba400acb8e460ab75e1d32e33e7
Chiro <ficosaputro@gmail.com>
2024-06-27 11:33:57 +0700

    Add files via upload
    Menambahkan file latihan c++ , pada pertemuan ke-1 semester 1

---
+++ AlproPraktikum1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Hello, world!" << endl;
+   return 0;
+}
+// output program ini adalah teks yang menampilkan hello,world!
---
+++ Latihan01c.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Data Mahasiswa";
+    cout << "===============================";
+    cout << "Nama       : Awalina Damayanti";
+    cout << "Alamat     : Jl. Emas 14 ";
+    cout << "Kota       : Semarang";
+    cout << "Propinsi   : Jawa Tengah";
+
+    return 0;
+}
+// output program ini adalah teks dokuemn yang sudah diketik diatas namun masih berantakan karena teks disambung
