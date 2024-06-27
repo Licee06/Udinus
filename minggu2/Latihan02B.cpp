ab1ff6962a1513f23b6a2199a478c8b9e703ea64
Chiro <ficosaputro@gmail.com>
2024-06-27 12:50:12 +0700

    Add files via upload
    Menambah file latihan minggu 2, pertemuan ke-2 pada semester 1

---
+++ minggu2/Latihan02B.cpp
@@ -0,0 +1,18 @@
+//NIM   : A12.2023.07058
+//Nama  : Fico Aldi Saputro
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+const int p = 8;
+
+int main ()
+{
+    int l (50);
+
+    int Luas = p * l;
+    cout << " luas = " << Luas << endl;
+    getch();
+}
