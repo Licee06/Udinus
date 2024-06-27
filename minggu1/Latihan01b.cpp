d600fa28fe0fee5e563ee4aeec71d86776a04977
Chiro <ficosaputro@gmail.com>
2024-06-27 11:39:44 +0700

    Add files via upload

---
+++ minggu1/Latihan01b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "Karawang";
+    cout << "Kota";
+    cout << "Asri";
+    cout << "Karawang Ibu Kota Jawa Barat";
+    cout << "Karawang Menggunakan Bahasa Sunda";
+    return 0;
+}
+// output program ini adalah teks dokumen yang diketik diatas namun belum tersusun dengan rapih
